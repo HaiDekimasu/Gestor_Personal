9a4f30a97c19952e52f653933d4416404a925795
Haidekimasu <cadevillajefferson@gmail.com>
2023-12-29 22:22:54 -0300

    modified:   Gestor_Personal/settings.py

--- Gestor_Personal/settings.py
+++ Gestor_Personal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n!@q@f!^!4jbnbycu_j+$gf80sa0v4bnj8gi(1pqx!v*$f-=zi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jcadev.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
