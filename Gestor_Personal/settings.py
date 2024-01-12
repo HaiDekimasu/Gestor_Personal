9b95a59231579294d987a933788fc6f7cd1391bb
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-11 23:57:36 -0300

    revision29

--- Gestor_Personal/settings.py
+++ Gestor_Personal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-# import os
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n!@q@f!^!4jbnbycu_j+$gf80sa0v4bnj8gi(1pqx!v*$f-=zi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-# 'jcadev.pythonanywhere.com','localhost','127.0.0.1'
+ALLOWED_HOSTS = ['jcadev.pythonanywhere.com','localhost','127.0.0.1']
 
 # Application definition
 
@@ -118,16 +117,15 @@ USE_TZ = True
 
 
 
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Mantiene su configuración
 
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
     
-]
+# ]
 
 
 # Default primary key field type
--- Gestor_Personal/wsgi.py
+++ Gestor_Personal/wsgi.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -7,27 +6,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE' , 'Gestor_Personal.settings')
 
 application = get_wsgi_application()
-=======
-# import os
-# import sys
-
-# path = os.path.expanduser('~/Gestor-p/Gestor_Personal')
-# if path not in sys.path:
-#     sys.path.insert(0, path)
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'Gestor_Personal.settings'
-# from django.core.wsgi import get_wsgi_application
-# from django.contrib.staticfiles.handlers import StaticFilesHandler
-# application = StaticFilesHandler(get_wsgi_application())
->>>>>>> bee32cf (	modified:   Gestor_Personal/settings.py)
-=======
-import os
-import sys
-
-path = os.path.expanduser('~/Gestor-p/Gestor_Personal')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Gestor_Personal.settings'
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
->>>>>>> 298dd56 (	modified:   .vscode/settings.json)
