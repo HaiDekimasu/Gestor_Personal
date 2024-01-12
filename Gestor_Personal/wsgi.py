96d73808ed7f351f5540e03a1eed99749e046bf3
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-11 23:56:07 -0300

    revision28

--- Gestor_Personal/wsgi.py
+++ Gestor_Personal/wsgi.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -18,3 +19,15 @@ application = get_wsgi_application()
 # from django.contrib.staticfiles.handlers import StaticFilesHandler
 # application = StaticFilesHandler(get_wsgi_application())
 >>>>>>> bee32cf (	modified:   Gestor_Personal/settings.py)
+=======
+import os
+import sys
+
+path = os.path.expanduser('~/Gestor-p/Gestor_Personal')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Gestor_Personal.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
+>>>>>>> 298dd56 (	modified:   .vscode/settings.json)
